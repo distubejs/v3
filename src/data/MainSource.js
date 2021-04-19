274ea62509bb67154156aead22a90b7d0bb4bd9c
Skick <skick1234@gmail.com>
2021-04-19 23:29:03 +0700

    Better constructor for DisTube

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -5,6 +5,7 @@ export default new DocsSource({
   id: 'DisTube',
   name: 'DisTube',
   repo: 'skick1234/DisTube',
+  global: 'DisTube',
   defaultTag: 'v2',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
   tagFilter: () => false,
