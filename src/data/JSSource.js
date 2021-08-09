3879935f6b6c53e6fbadd7654a60b09f5c92baef
Skick <skick1234@gmail.com>
2021-08-09 13:58:00 +0700

    Update typescript constructor

--- src/data/JSSource.js
+++ src/data/JSSource.js
@@ -3,13 +3,13 @@ import DocsSource from "./DocsSource";
 
 const branchBlacklist = new Set(["docs", "js-docs", "v2"]);
 export default new DocsSource({
-  id: "TS DisTube",
-  name: "TS DisTube",
+  id: "JS DisTube",
+  name: "JS DisTube",
   repo: "skick1234/DisTube",
   global: "DisTube",
-  branch: "docs",
+  branch: "js-docs",
   defaultTag: "beta",
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith("dependabot/"),
-  tagFilter: tag => semver.gte(tag, "3.0.0") || semver.gte(tag, "3.0.0-beta.34"),
-  defaultFile: { category: "general", id: "welcome" },
+  tagFilter: tag => !semver.gte(tag, "3.0.0-beta.34"),
+  defaultClass: "DisTube",
 });
