13c97332f509a3ea46e4a8595ccdf6aa4fbf94b2
Skick <skick1234@gmail.com>
2021-08-09 14:27:03 +0700

    Update JSSource.js

--- src/data/JSSource.js
+++ src/data/JSSource.js
@@ -1,15 +1,14 @@
 import semver from "semver";
 import DocsSource from "./DocsSource";
 
-const branchBlacklist = new Set(["docs", "js-docs", "beta", "alpha"]);
 export default new DocsSource({
-  id: "JS DisTube",
+  id: "js-disTube",
   name: "JS DisTube",
   repo: "skick1234/DisTube",
   global: "DisTube",
   branch: "js-docs",
   defaultTag: "beta",
-  branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith("dependabot/"),
-  tagFilter: tag => !semver.gte(tag, "3.0.0-beta.34"),
+  branchFilter: branch => branch === "v2",
+  tagFilter: tag => tag === "3.0.0-beta.33",
   defaultClass: "DisTube",
 });
