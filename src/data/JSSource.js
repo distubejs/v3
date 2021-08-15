d1a221b6c0dfba662414c467955b77020109d3fb
Skick <skick1234@gmail.com>
2021-08-16 05:51:11 +0700

    Change default docs to v2

--- src/data/JSSource.js
+++ src/data/JSSource.js
@@ -1,13 +1,15 @@
+import semver from "semver";
 import DocsSource from "./DocsSource";
 
+const branchBlacklist = new Set(["docs", "js-docs", "v2", "alpha", "stable"]);
 export default new DocsSource({
-  id: "JS-DisTube",
-  name: "JS DisTube",
+  id: "DisTube",
+  name: "TypeScript DisTube",
   repo: "skick1234/DisTube",
   global: "DisTube",
-  branch: "js-docs",
-  defaultTag: "v2",
-  branchFilter: branch => branch === "v2",
-  tagFilter: tag => tag === "3.0.0-beta.33",
-  defaultClass: "DisTube",
+  branch: "docs",
+  defaultTag: "beta",
+  branchFilter: branch => !branchBlacklist.has(branch),
+  tagFilter: tag => semver.gte(tag, "3.0.0") || semver.gte(tag, "3.0.0-beta.34"),
+  defaultFile: { category: "general", id: "welcome" },
 });
